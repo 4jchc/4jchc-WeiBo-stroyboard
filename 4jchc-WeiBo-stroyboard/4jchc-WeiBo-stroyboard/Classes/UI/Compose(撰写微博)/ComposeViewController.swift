470b3e752edbd4d450f10928a9845bf4b6a168fb
4jchc <jiangjin5y24@163.com>
2016-01-07 18:22:51 +0800

    4.7监听键盘关闭通知

--- 4jchc-WeiBo-stroyboard/4jchc-WeiBo-stroyboard/Classes/UI/Compose(撰写微博)/ComposeViewController.swift
+++ 4jchc-WeiBo-stroyboard/4jchc-WeiBo-stroyboard/Classes/UI/Compose(撰写微博)/ComposeViewController.swift
@@ -40,18 +40,30 @@ class ComposeViewController: UIViewController ,UITextViewDelegate{
         placeHolderLabel!.hidden = !textView.text.isEmpty
         sendButton.enabled = !textView.text.isEmpty
     }
+    /// 滚动视图开始被拖拽
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        textView.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 文本框默认不支持滚动，设置此属性后，能够滚动！
         textView.alwaysBounceVertical = true
         textView.addSubview(placeHolderLabel!)
+        // 设置文本框的默认焦点
+        textView.becomeFirstResponder()
         // 添加观察者，监听键盘框架的变化
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardFrameChanged:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardFrameChanged:", name: UIKeyboardWillHideNotification, object: nil)
     }
     
     deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillChangeFrameNotification, object: nil)
+        
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+//        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillChangeFrameNotification, object: nil)
+//        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
         
     }
     
@@ -59,11 +71,18 @@ class ComposeViewController: UIViewController ,UITextViewDelegate{
     func keyboardFrameChanged(notification: NSNotification) {
         print(notification)
         
-        let rect = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
-        let duration = (notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        var height: CGFloat = 0
+        var duration = 0.25
         
-        toolBarBottomConstraint.constant = rect.size.height
+        if notification.name == UIKeyboardWillChangeFrameNotification {
+            let rect = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+            height = rect.size.height
+            
+            duration = (notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+        }
         
+        toolBarBottomConstraint.constant = height
+
         UIView.animateWithDuration(duration) {
             
             self.view.layoutIfNeeded()
@@ -71,11 +90,7 @@ class ComposeViewController: UIViewController ,UITextViewDelegate{
     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+
 
 
 }
