03a3c0f1195addec93946c680e9aedf6e76de6c9
4jchc <jiangjin5y24@163.com>
2016-01-07 17:19:32 +0800

    4.6修改占位文本标签

--- 4jchc-WeiBo-stroyboard/4jchc-WeiBo-stroyboard/Classes/UI/Compose(撰写微博)/ComposeViewController.swift
+++ 4jchc-WeiBo-stroyboard/4jchc-WeiBo-stroyboard/Classes/UI/Compose(撰写微博)/ComposeViewController.swift
@@ -15,19 +15,37 @@ class ComposeViewController: UIViewController ,UITextViewDelegate{
         
         dismissViewControllerAnimated(true, completion: nil)
     }
-    
-    @IBOutlet weak var placeHolderLabel: UILabel!
-    
+    /// 发送按钮
+    @IBOutlet weak var sendButton: UIBarButtonItem!
+    /// 占位文本 － 注意：懒加载的对象属性，不要使用 weak
+    lazy var placeHolderLabel: UILabel? = {
+        let l = UILabel()
+        l.text = "分享新鲜事..."
+        l.font = UIFont.systemFontOfSize(18)
+        l.textColor = UIColor.lightGrayColor()
+        l.frame = CGRectMake(5, 8, 0, 0)
+        // 可以根据文本的内容大小，自动调整
+        l.sizeToFit()
+        
+        return l
+    }()
+    @IBOutlet weak var textView: UITextView!
+
     @IBOutlet weak var toolBarBottomConstraint: NSLayoutConstraint!
     
     
     func textViewDidChange(textView: UITextView) {
         print(textView.text)
-        placeHolderLabel.hidden = !textView.text.isEmpty
+       
+        placeHolderLabel!.hidden = !textView.text.isEmpty
+        sendButton.enabled = !textView.text.isEmpty
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 文本框默认不支持滚动，设置此属性后，能够滚动！
+        textView.alwaysBounceVertical = true
+        textView.addSubview(placeHolderLabel!)
         // 添加观察者，监听键盘框架的变化
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardFrameChanged:", name: UIKeyboardWillChangeFrameNotification, object: nil)
     }
